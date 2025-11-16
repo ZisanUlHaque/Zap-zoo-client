004971b159805414cfdb93fbf0abed0890fb8788
Zisan <mdzisanulhaque989@gmail.com>
2025-11-16 17:37:54 +0600

    project setup complete

---
+++ src/routes/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-amber-700">Hello World</div>,
+  },
+]);
